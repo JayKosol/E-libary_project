6bac701f4a675fc12eef117a64162a0389d24232
blackshadowzz <Rymang66@gmail.com>
2022-07-10 22:07:31 +0700

    Update users and connection

---
+++ Asset/dbconnection.php
@@ -0,0 +1,16 @@
+<?php 
+     define("DNS","mysql:host=localhost;dbname=elibraryms;port=3306;");
+     define("USERNAME",'root');
+     define("PASSWORD",'');
+
+     $options=[
+          PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
+          PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+     ];
+     try{
+          $conn=new PDO(DNS,USERNAME,PASSWORD,$options);
+     }catch(Exception $ex){
+          die($ex->getMessage());
+     }
+
+?>
