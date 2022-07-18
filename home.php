3b8d3069dee6f9e35f2271a9d7c2b07481dcf944
JayKosol <Kosalaction007@gmail.com>
2022-07-19 00:03:41 +0700

    main

--- home.php
+++ home.php
@@ -4,9 +4,10 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <?php require_once "./Asset/boostrap.php";
+     <?php include_once "./Asset/boostrap.php";
            include_once "./Asset/dbconnection.php";
-           include "./Asset/font.php";
+           include_once "./Asset/font.php";
+           include_once "./Users/function.php";
      ?>
      <link rel="stylesheet" href="./Asset/css/style.css">
      <title>Home Page</title>
@@ -20,51 +21,51 @@
             </div>
             <i class='bx bx-menu' id="btn"></i>
       </div>
-      <ul class="nav-list">
+      <ul class="nav-list tabs">
                   <li>
                         <a href="#">
                               <i class='i bx bx-search'></i>
                               <input type="text" id="search" placeholder="Search">
                         </a>
                         <!-- <i class='tool' ></i> -->
-                  </li>
-                  <li id="btn_dashboard">
-                        <a href="#">
+                  </li> 
+                  <li class="tab is-active">
+                        <a href="#" data-switcher data-tab="1">
                               <i class='bx bxs-dashboard i' ></i>
                               <span class="link_name">Dashboard</span>
                         </a>
                         <!-- <i class='tool' ></i> -->
                   </li>
-                  <li id="btn_category">
-                        <a href="#">
+                  <li  class="tab">
+                        <a href="#" data-switcher data-tab="2">
                               <i class='bx bx-category i' ></i>
                               <span class="link_name">Category</span>
                         </a>
                         <!-- <i class='tool' ></i> -->
                   </li>
-                  <li id="btn_author">
-                        <a href="#">
+                  <li  class="tab">
+                        <a href="#" data-switcher data-tab="3">
                               <i class='bx bxs-user-detail i' ></i>
                               <span class="link_name">Author</span>
                         </a>
                         <!-- <i class='tool' ></i> -->
                   </li>
-                  <li id="btn_book">
-                        <a href="#">
+                  <li  class="tab">
+                        <a href="#" data-switcher data-tab="4">
                               <i class='bx bxs-book i' ></i>
                               <span class="link_name">Book</span>
                         </a>
                         <!-- <i class='tool' ></i> -->
                   </li>
-                  <li id="btn_issue">
-                        <a href="#">
+                  <li  class="tab">
+                        <a href="#" data-switcher data-tab="5">
                               <i class='bx bxs-key i' ></i>
                               <span class="link_name">Issue</span>
                         </a>
                         <!-- <i class='tool' ></i> -->
                   </li>
-                  <li id="btn_user">
-                        <a href="#">
+                  <li  class="tab">
+                        <a href="#" data-switcher data-tab="6">
                               <i class='bi bi-people-fill' ></i>
                               <span class="link_name">Users</span>
                         </a>
@@ -92,17 +93,19 @@
      </div>
      
 
-     <div class="home_content">
-            <div class="dashboard">
+     <div id="home_content" class="pages">
+            <div class="page is-active" data-page="1">
                   <?php include "./Asset/dashboard.php" ?>
             </div>
-            <div class="user">
+            <div class="page" data-page="2"></div>
+            <div class="page" data-page="3"></div>
+            <div class="page" data-page="4">
+                  <?php include_once "./book/read.book.php" ?>
+            </div>
+            <div class="page" data-page="5"></div>
+            <div class="page" data-page="6">
                   <?php include_once "./Users/read.user.php" ?>
             </div>
-            <div class="issue"></div>
-            <div class="book"></div>
-            <div class="author"></div>
-            <div class="category"></div>
      </div>
      <script src="./Asset/home.js"></script>
 </body>
