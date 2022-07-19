753f482031d9ce43ec27b444bd3a866972afe812
JayKosol <Kosalaction007@gmail.com>
2022-07-19 10:48:53 +0700

    main

--- category/read.cate.php
+++ category/read.cate.php
@@ -1,20 +1,5 @@
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Category page</title>
-     <?php 
-          include_once './../Asset/boostrap.php';
-          require_once './../Asset/dbconnection.php';
-          include_once './../Users/function.php';
-     ?>
-</head>
-<body>
-
 <?php 
      
           
@@ -218,5 +203,3 @@
         </div>
         
      </div>
-</body>
-</html>
