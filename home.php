d166545942c7b950ee2f0b2f0991d2f36cbf9874
JayKosol <Kosalaction007@gmail.com>
2022-07-26 22:12:14 +0700

    main

--- home.php
+++ home.php
@@ -73,28 +73,43 @@
                   </li>
             </ul>
 
-            <!-- @profile -->
-            <div class="profile-content">
-                  <div class="profile">
-                        <img src="./img/default.png" alt="">
-                        <div class="profile-detail">
-                              <div class="name_job">
-                                    <div class="name">Username</div>
-                                    <div class="job">
-                                          <span>Administrator</span>
-                                          <i class='bx bx-log-out' id="btn_logout"></i>
-                                    </div>
-                                         
-                              </div>
-                              
-                        </div>
-                  </div>
-            </div>
+            
      </div>
      
 
      <div id="home_content" class="pages">
-            <?php include_once "./Asset/dashboard.php" ?>
+            <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+            <a class="navbar-brand" href="#">Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                  </li>
+                  <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                  </li>
+                  </ul>
+            </div>
+            </div>
+            </nav>   
      </div>
      <script src="./Asset/home.js"></script>
 </body>
