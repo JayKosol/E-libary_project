192c79a6f799873f63382c3b49f40315bf709230
blackshadowzz <Rymang66@gmail.com>
2022-08-05 22:58:50 +0700

    Update

--- home.php
+++ home.php
@@ -159,13 +159,101 @@
                         </div>
 
                   </div>
+                  <?php 
+          
+                  if(isset($_GET['limit'])){
+                        $lim=$_GET['limit'];
+                  }else{
+                        $lim=""; 
+                  }
+                  if($lim==0 || empty($lim)){
+                        $limit=5;
+                  }else{
+                        $limit=$lim;
+                  }
+                  //echo $limit;
+                  if(isset($_GET['page'])){
+                        $pag=$_GET['page'];
+                  }else{
+                        $pag="";
+                  }
+                  if($pag==0 || empty($pag)){
+                        $page=0;
+                  }else{
+                        $page=$pag;
+                  }
+                  if($page == 0){
+                        $pagination=1;
+                  }else{
+                        $pagination=$page;
+                  }
+                  $offset= ceil($pagination * $limit)-$limit;
 
+                  if(isset($_GET['s'])){
+                        $s=$_GET['s'];
+                  }else{
+                        $s="";
+                  }
+
+                  if(isset($_GET['sortBy'])){
+                        $sort=$_GET['sortBy'];
+                  }else{
+                        $sort="";
+                  }
+                  if(empty($sort)){
+                        $sortBy='DESC';
+                  }elseif($sort==='Old'){
+                        $sortBy='ASC';
+                  }else{
+                        $sortBy='DESC';
+                  }
+                  ?>
                   <!-- @table-bookcontent -->
                   <div class="table-book">
+
                         <h3>Book</h3>
+                        <div class="row g-6">
+                         <div class="col-md-8">
+                              <form id="search_box">
+                                   <div class="input-group d-flex">
+                                        <label for="search" class="input-group-text">Search</label>
+                                        <input type="search" value="<?= $s; ?>" name="search" id="search_input" placeholder="Search by name ..." class="form-control me-2">
+                                        <button class="btn btn-primary">Search</button>
+                                   </div>
+                              </form>
+                             
+                              
+                         </div>
+                         <div class="col-md-2">
+                              
+                              <div class="input-group" style="width: 170px;float:right;">
+                                   <label for="sortBy" class="input-group-text">Sort By</label>
+                                   <select class="form-select" id="page_sort">
+                                        <option style="display: none;" ><?php echo $sort === ""?"New": $sort; ?></option>
+                                        <option >New</option>
+                                        <option >Old</option>
+                                   </select>
+                                   
+                              </div>
+                         </div>
+                         
+                         <div class="col" >
+                              <div class="input-group" style="width: 140px;float:right;" >
+                                   <label for="search"  class="input-group-text">Page/</label>
+                                   <select class="form-select" id="page_limit">
+                                        <option style="display: none;" ><?php echo $limit; ?></option>
+                                        <option >5</option>
+                                        <option >15</option>
+                                        <option >25</option>
+                                        <option >50</option>
+                                   </select>
+                              </div>
+                              
+                         </div>
+                    </div>
                         <table class="table table-bordered bg-light shadow">
-                              <thead class="table-dark">
-                                    <tr>
+
+                                    <tr class="table-dark">
                                           <th scope="col">ID</th>
                                           <th scope="col">Book Title</th>
                                           <th scope="col">Category</th>
@@ -173,15 +261,18 @@
                                           <th scope="col">Language</th>
                                           <th scope="col">Image</th>
                                           <?php
-                                                $sql="SELECT b.*,a.authorName,c.categoryName FROM books b INNER JOIN authors a ON b.authorsId=a.authorId INNER JOIN category c ON b.categoryId=c.categoryId ORDER BY bookId DESC";
+                                                $sql="SELECT b.*,a.authorName,c.categoryName FROM books b INNER JOIN authors a ON b.authorsId=a.authorId INNER JOIN category c ON b.categoryId=c.categoryId  
+                                                WHERE LOWER(bookTitle) LIKE LOWER('%$s%') ORDER BY bookId $sortBy LIMIT $limit OFFSET $offset";
+                                                $queryCount="SELECT * FROM books WHERE LOWER(bookTitle) LIKE LOWER('%$s%')";
+                                                $reCount=$conn->query($queryCount);
+                                                $count=$reCount->rowCount();
+                                                $countPage=ceil($count /$limit);
 
                                                 $stm=$conn->query($sql);
                                                 if($stm->rowCount()>0){
                                                       while($book=$stm->fetch()):    
                                           ?>
                                     </tr>
-                              </thead>
-                              <tbody>
                               <tr>
 
                                     <td><?= $book['bookId']  ?></td>
@@ -190,7 +281,7 @@
                                     <td><?= $book['authorName']  ?></td>
                                     <td><?= $book['languages']  ?></td>
                                     <td>
-                                          <img style="width: 50px;height:60px;" src="<?= $book['photos']; ?>" />
+                                          <img style="width: 55px;height:70px;" src="<?php echo $book['photos']; ?>"/>
                                           
                                     </td>
                                     <?php 
@@ -201,11 +292,33 @@
                                         }
                                     ?>
                               </tr>
-                              </tbody>
+                              
                         </table>
+                        <nav aria-label="Page navigation example ">
+                    <ul class="pagination justify-content-center mt-2">
+                         <li class="page-item <?php echo $pagination ==1 ? 'disabled':''; ?>">
+                              <a class="page-link" href="./home.php?limit=<?php echo $limit?>&page=<?php echo $pagination-1?>">Previous</a>
+                         </li>
+
+                         <?php
+                              for($i=1; $i<=$countPage;$i++){
+                                   ?>
+                                   <li class="page-item <?php echo $pagination ==$i ?'active':''; ?>"><a class="page-link"
+                                   href="./home.php?limit=<?php echo $limit?>&page=<?php echo $i;?>"><?php echo $i ?></a></li>
+                                   <?php
+                              }
+                         ?>
+                         
+                    
+                         
+                         <li class="page-item <?php echo $pagination >= $countPage ? 'disabled':''; ?>">
+                              <a class="page-link" href="./home.php?limit=<?php echo $limit?>&page=<?php echo $pagination+1?>">Next</a>
+                         </li>
+                    </ul>
+               </nav>
                   </div>
                   <!-- @table-Author -->
-                  <div class="table-author">
+                  <!-- <div class="table-author">
                         <h3>Author</h3>
                         <table class="table border bg-light shadow">
                               <thead class="table-dark">
@@ -255,12 +368,38 @@
                               </tr>
                               </tbody>
                         </table>
-                  </div>
+                  </div> -->
 
             </div>
 
             
      </div>
      <script src="./Asset/home.js"></script>
+     <script>
+          const pageLilit=document.querySelector("#page_limit");
+          pageLilit.addEventListener("change", function(e){
+               const value=e.currentTarget.value;
+               window.location.href=`home.php?limit=${value}&page=0`;
+          });
+
+          const searchBar=document.querySelector("#search_box");
+          const searchValue=searchBar.querySelector("#search_input");
+          searchBar.addEventListener("submit",function(e){
+               e.preventDefault();
+               window.location.href=`home.php?limit=<?= $limit; ?>&page=0&s=${searchValue.value}&sortBy=<?= $sort ?>`;
+
+          });
+          searchValue.addEventListener("input",function(e){
+               const value=e.currentTarget.value;
+               if(value.length===0) window.location.href=`home.php?limit=<?= $limit; ?>&page=0&s=`;
+          });
+
+          const sortBy=document.querySelector("#page_sort");
+          sortBy.addEventListener("change",function(e){
+               const value=e.currentTarget.value;
+               window.location.href=`home.php?limit=<?= $limit; ?>&page=0&s=${searchValue.value}&sortBy=${value}`;
+          });
+
+     </script>
 </body>
 </html>
